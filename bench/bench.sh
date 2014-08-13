9632004ce776d980f310cec3e29dfc66bb4f4990
Masahiro Nagano <kazeburo@gmail.com>
2014-08-13 16:52:27 +0900

    fix command

--- bench/bench.sh
+++ bench/bench.sh
@@ -2,5 +2,5 @@
 set -e
 CDIR=$(cd $(dirname $0) && pwd)
 cd $CDIR
-./bench --workload 1 --init /home/isu-user/isucon/init.sh 2>&1 | tee ./data/latest
+./bench benchmark --workload 1 --init /home/isu-user/isucon/init.sh 2>&1 | tee ./data/latest
 
